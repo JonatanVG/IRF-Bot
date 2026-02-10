6a47b71221e93aff74f779ba2e41fe3a62773f57
JonatanVG <jonatangrondahl@outlook.com>
2026-02-10 11:13:46 +0100

    Main 2.0.2 SyntaxFix

--- bot_managment/shutdown.py
+++ bot_managment/shutdown.py
@@ -6,7 +6,7 @@ async def shutdown(ctx, name: str = ""):
     if bot.user.name == name:
         await ctx.send("Shutting down...") # Sends a message on discord that the bot is shutting down.
         print("Shutting down...") # This is important to make sure the bot doesn't relaunch. (Only necessary if you're using github actions.)
-        stop_koyeb_service() # Stops the koyeb service to make sure the bot doesn't relaunch. (Only necessary if you're using koyeb.)
+        await stop_koyeb_service() # Stops the koyeb service to make sure the bot doesn't relaunch. (Only necessary if you're using koyeb.)
         await bot.close() # Shuts down the bot.
     else:
         None
